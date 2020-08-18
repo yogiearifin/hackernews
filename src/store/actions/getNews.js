39f35f12c2fdb6c483a4a3e9dfed575fb348498a
Yogie Arifin <yogiearifin@yahoo.co.id>
2020-08-18 11:15:58 +0700

    [Yogie] - minor fix

--- src/store/actions/getNews.js
+++ src/store/actions/getNews.js
@@ -7,13 +7,17 @@ export const storyUrl = `${baseUrl}item/`;
 
 export const getStory = () => async dispatch => {
     try {
-        const response = await fetch (newStoriesUrl);
+        const response = await axios.get (newStoriesUrl);
         if (response.ok === false) {
             throw new Error("Response Error")
         }
-        const json = await response.json();
-        const promises = json.map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
-            response => response.json()
+        console.log("json", response)
+        const json = await response.data;
+        const promises = json.map((id) =>  axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        .then(
+            response => {
+                console.log(response)
+                return response.data}
         ))
         const result = await Promise.all(promises)
         dispatch({
@@ -26,28 +30,6 @@ export const getStory = () => async dispatch => {
         console.log(error)
     }
 };
-
-// export const getBestStory = () => async dispatch => {
-//     try {
-//         const response = await axios.fetch (bestStoriesUrl);
-//         if (response.ok === false) {
-//             throw new Error("Response Error")
-//         }
-//         const json = await response.json();
-//         const promises = json.map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
-//             response => response.json()
-//         ))
-//         const result = await Promise.all(promises)
-//         dispatch({
-//             type: "GET_BEST_STORY",
-//             payload:result
-//         })
-//         console.log("result best", result)
-//     }
-//     catch(error) {
-//         console.log(error)
-//     }
-// };
 
 export const getBestStory = () => async dispatch => {
     try {
@@ -77,20 +59,24 @@ export const getBestStory = () => async dispatch => {
 
 export const jobStory = () => async dispatch => {
     try {
-        const response = await fetch (jobStoriesUrl);
+        const response = await axios.get (jobStoriesUrl);
         if (response.ok === false) {
             throw new Error("Response Error")
         }
-        const json = await response.json();
-        const promises = json.map(id => fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
-            response => response.json()
+        console.log("json", response)
+        const json = await response.data;
+        const promises = json.map((id) =>  axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+        .then(
+            response => {
+                console.log(response)
+                return response.data}
         ))
         const result = await Promise.all(promises)
         dispatch({
             type: "GET_JOB_STORY",
             payload:result
         })
-        console.log("result job", result)
+        console.log("result", result)
     }
     catch(error) {
         console.log(error)
